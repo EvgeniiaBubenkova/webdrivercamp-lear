a090705d9d9d341b79a6008583801564416b8cba
EvgeniiaBubenkova <bubenkova87@gmail.com>
2024-05-17 22:01:15 -0500

    Printing of modified copy of the list and the original list as well

--- ning-python/python_data_srtuctures/4_copy_update.py
+++ ning-python/python_data_srtuctures/4_copy_update.py
@@ -3,10 +3,10 @@
 list_ = [5, 4, 3, 2, 1]
 index = 1
 element_to_replace = 5
-
+copy=list_[:]
 if index >= 0 and index < len(list_):
-    list_[index] = element_to_replace
+    copy[index] = element_to_replace
 
-print(list_)
-
+print("Copy:", copy)
+print("Original:", list_)
 
