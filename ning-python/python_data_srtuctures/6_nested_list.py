caeee0f474acf3b482b359d621ebb8743d88a687
EvgeniiaBubenkova <bubenkova87@gmail.com>
2024-05-18 00:31:47 -0500

    Function for printing matrix (nested_list)

---
+++ ning-python/python_data_srtuctures/6_nested_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+def print_matrix(matrix):
+    for row in matrix:
+        for element in row:
+            print(element, end=" ") 
+        print()
+
+print_matrix(matrix)
+
