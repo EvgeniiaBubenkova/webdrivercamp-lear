07a8e32c90fedc546e2fe4586d2860a63a9b70d7
EvgeniiaBubenkova <bubenkova87@gmail.com>
2024-05-26 16:49:22 -0500

    Function that returns a key with the biggest integer value

--- ning-python/python-data-srtuctures-continue/10_max_value.py
+++ ning-python/python-data-srtuctures-continue/10_max_value.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 def max_value(d):
-    def max_num(dict_):
-        if not dict_:
-            return None
-        max_num = 0
-        for i in dict_.values():
-            if i > max_num:
-                max_num = i
-        return max_num
+    if d:
+      return max(d, key=lambda k: d[k])
 
-    if __name__=="__main__":
-        dict_ = {'Apple': 13, 'Pear': 1, 'Plum': 20, 'Grape': 10}
-        max_key = max_value(dict_)
-        print(f"Max number - {max_key}")
+if __name__=="__main__":
+    dict_ = {'Apple': 13, 'Pear': 1, 'Plum': 20, 'Grape': 10}
+    max_key = max_value(dict_)
+    print(f"Max number - {max_key}")
 
-        max_key = max_value(None)
-        print(f"Max number - {max_key}")
+    max_key = max_value(None)
+    print(f"Max number - {max_key}")
 
-max_value(None)
- 
+
