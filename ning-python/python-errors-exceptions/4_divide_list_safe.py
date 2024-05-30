28ba90d6444f7ce457e6ce024fdbbc11293cc1b4
EvgeniiaBubenkova <bubenkova87@gmail.com>
2024-05-29 21:01:29 -0500

    Divide a list

--- ning-python/python-errors-exceptions/4_divide_list_safe.py
+++ ning-python/python-errors-exceptions/4_divide_list_safe.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 def divide_list_safe(list_1, list_2, list_len):
-  
-
+    new_list = []
+    for i in range(list_len):
+        try:
+            result = list_1[i] / list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+    return new_list
 
 if __name__ == "__main__":
     list_1 = [9, 2, 6]
@@ -14,3 +28,4 @@ if __name__ == "__main__":
     list_2 = ["one", 0, 1, 2, 7]
     res = divide_list_safe(list_1, list_2, max(len(list_1), len(list_2)))
     print(res)
+
